a12ced592c495a9e4f80920e9b61d9fa96cb343f
Aaron <aaron@huvitz.com>
2025-07-30 08:13:38 +0900

    alp Serialization

--- src/cuappFeather/main.cpp
+++ src/cuappFeather/main.cpp
@@ -478,57 +478,7 @@ int main(int argc, char** argv)
 				ply.Deserialize(resource_file_name_ply);
 				//ply.SwapAxisYZ();
 
-				vector<PointPNC> points;
-				for (size_t i = 0; i < ply.GetPoints().size() / 3; i++)
-				{
-					auto px = ply.GetPoints()[i * 3];
-					auto py = ply.GetPoints()[i * 3 + 1];
-					auto pz = ply.GetPoints()[i * 3 + 2];
-
-					if (0 == px && 0 == py && 0 == pz)
-					{
-						if (false == foundZero)
-						{
-							foundZero = true;
-						}
-						else
-						{
-							continue;
-						}
-					}
-
-					auto nx = ply.GetNormals()[i * 3];
-					auto ny = ply.GetNormals()[i * 3 + 1];
-					auto nz = ply.GetNormals()[i * 3 + 2];
-
-					if (false == ply.GetColors().empty())
-					{
-						if (ply.UseAlpha())
-						{
-							auto cx = ply.GetColors()[i * 4];
-							auto cy = ply.GetColors()[i * 4 + 1];
-							auto cz = ply.GetColors()[i * 4 + 2];
-							auto ca = ply.GetColors()[i * 4 + 3];
-
-							points.push_back({ {px, py, pz}, {nx, ny, nz}, {cx, cy, cz} });
-						}
-						else
-						{
-							auto cx = ply.GetColors()[i * 3];
-							auto cy = ply.GetColors()[i * 3 + 1];
-							auto cz = ply.GetColors()[i * 3 + 2];
-
-							points.push_back({ {px, py, pz}, {nx, ny, nz}, {cx, cy, cz} });
-						}
-					}
-					else
-					{
-						points.push_back({ {px, py, pz}, {nx, ny, nz}, {1.0f, 1.0f, 1.0f} });
-					}
-				}
-				alog("PLY %llu points loaded\n", points.size());
-
-				alp.AddPoints(points);
+				alp.FromPLY(ply);
 				alp.Serialize(resource_file_name_alp);
 			}
 
